bd398bd4206d29631646c715223d602a1d0cebb6
Myanmar788 <101880532+Myanmar788@users.noreply.github.com>
2022-03-19 20:02:49 +0800

    Add files via upload

---
+++ MMApyarLover.js
@@ -0,0 +1,3 @@
+function isMyApp(){if(navigator.userAgent.match(/wv/i)){if(navigator.userAgent.match(/com.myanmar.apyarlover/i)){return true;}
+return false;}}
+if(!isMyApp()){try{window.location.replace("https://randyfukgg.blogspot.com/2022/03/download-mm-apyar-lover-app.html?m=1");}catch(e){alert("Error");}}else{}
